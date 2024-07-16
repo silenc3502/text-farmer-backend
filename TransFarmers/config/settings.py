cbfe91e59b8cb6fd03fbce067dcb0f7af2d9f62f
silenc3502 <gcccompil3r@gmail.com>
2024-07-16 23:12:04 +0900

    change host

--- TransFarmers/config/settings.py
+++ TransFarmers/config/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "NAME": os.getenv('DATABASE_NAME'),
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': 'localhost',
+        'HOST': os.getenv('DATABASE_HOST'),
         'PORT': '3306',
     }
 }
